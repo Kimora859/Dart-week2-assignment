16aa73d78a2552267cdcda5576011376b918e210
kimora859 <ogazichinwendu864@gmail.com>
2024-05-19 14:50:30 +0100

    Dart week 2 assignment

---
+++ wk2Assg.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Number datatpe
+  // integer for whole numbers
+  int age = 22;
+  print("My age is $age.");
+
+  // double for decimal numbers
+  double pi = 3.142857;
+  print("The value of pi is $pi.");
+
+  // String datatype
+  String fullName = "Alexander Amos";
+  print("My full name is $fullName.");
+
+  // Bolean datatype
+  bool isTrue = true;
+  print("The answer to the question is $isTrue.");
+
+  // List datatype
+  List colors = ['red', 'blue', 'yellow', 'green'];
+  print("These are the list of my favorite colors: $colors");
+
+  // Map datatype
+  Map cars = {
+    "BMW": "red",
+    "Toyota": "silver",
+    "Mercedes": "black",
+  };
+  print("These are my favorite cars : $cars.");
+  print("The color of BMW is ${cars["BMW"]}.");
+}
